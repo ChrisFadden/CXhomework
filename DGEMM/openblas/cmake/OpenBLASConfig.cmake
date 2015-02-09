268bdc0735a2968a4351b37409bbbc2c197fff4d
Chris Fadden <chris_fadden@yahoo.com>
2015-02-09 13:34:27 -0500

    Added DGEMM time

---
+++ DGEMM/openblas/cmake/OpenBLASConfig.cmake
@@ -0,0 +1,2 @@
+SET(OpenBLAS_INCLUDE_DIRS /nethome/echow7/cx4220/openblas/include)
+SET(OpenBLAS_LIBRARIES /nethome/echow7/cx4220/openblas/lib/libopenblas.so)
---
+++ DGEMM/timeDGEMM.cpp
@@ -0,0 +1,75 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "sys/time.h"
+#include "time.h"
+#include <iostream>
+
+extern void dgemm_(char*, char*, int*, int*,int*, double*, double*, int*, double*, int*, double*, double*, int*);
+extern void openblas_set_numthreads(int);
+
+
+int main(int argc, char* argv[])
+{
+  int i;
+  printf("test!\n");
+  if(argc<4){
+    printf("Input Error\n");
+    return 1;
+  }
+
+  int m = atoi(argv[1]);
+  int n = atoi(argv[2]);
+  int k = atoi(argv[3]);
+  int sizeofa = m * k;
+  int sizeofb = k * n;
+  int sizeofc = m * n;
+  char ta = 'N';
+  char tb = 'N';
+  double alpha = 1.2;
+  double beta = 0.001;
+
+  struct timeval start,finish;
+  double duration;
+
+  double* A = (double*)malloc(sizeof(double) * sizeofa);
+  double* B = (double*)malloc(sizeof(double) * sizeofb);
+  double* C = (double*)malloc(sizeof(double) * sizeofc);
+
+  srand((unsigned)time(NULL));
+
+  for (i=0; i<sizeofa; i++)
+    A[i] = i%3+1;//(rand()%100)/10.0;
+
+  for (i=0; i<sizeofb; i++)
+    B[i] = i%3+1;//(rand()%100)/10.0;
+
+  for (i=0; i<sizeofc; i++)
+    C[i] = i%3+1;//(rand()%100)/10.0;
+ 
+  /****************************
+   * Time with different number of threads
+   ***************************/
+
+  for(int i = 1; i <= 36; i++)
+  {
+    openblas_set_num_threads(i);
+    gettimeofday(&start, NULL);
+    dgemm_(&ta, &tb, &m, &n, &k, &alpha, A, &m, B, &k, &beta, C, &m);
+    gettimeofday(&finish, NULL);
+
+    duration = ((double)(finish.tv_sec-start.tv_sec)*1000000 + (double)(finish.tv_usec-start.tv_usec)) / 1000000;
+  
+  }
+
+  
+  free(A);
+  free(B);
+  free(C);
+  return 0;
+}
+
+
+
+
+
+
